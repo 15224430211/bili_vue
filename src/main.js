0a71eaeadbed7cfa59831ce1f710c1c1b4a3f676
Dr.Lemon <512111519@qq.com>
2019-04-06 01:18:57 +0800

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false;
-
-// Object.defineProperty(Vue.prototype, '$http', {
-//   value: axios
-// });
 
 new Vue({
   router,
